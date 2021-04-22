52a8296308f7240fe8c8643c44a5507da46df271
Dreamy Cecil <stalker27production@gmail.com>
2021-04-22 20:19:13 +0300

    Add global class patch config to auto-patch the most common classes.

--- Sources/EntitiesMP/Common/Patches/PatchFunctions.cpp
+++ Sources/EntitiesMP/Common/Patches/PatchFunctions.cpp
@@ -44,8 +44,17 @@ void LoadClassPatchConfig(CTString strWorld) {
 
   HookConfigFunctions();
 
-  // get level config
+  // get global config
   CTString strConfigFile;
+  strConfigFile.PrintF("LevelPatches\\GlobalClasses.json");
+
+  // load if exists
+  if (!FileExists(strConfigFile) || ParseConfig(strConfigFile, _cbConfig) != DJSON_OK) {
+    WarningMessage("Couldn't parse global class patch config \"%s\"!", strConfigFile);
+    _cbConfig.Clear();
+  }
+
+  // get level config
   strConfigFile.PrintF("LevelPatches\\Classes\\%s.json", strWorld);
 
   // no config
@@ -53,11 +62,17 @@ void LoadClassPatchConfig(CTString strWorld) {
     return;
   }
 
-  // can't parse the config
-  if (ParseConfig(strConfigFile, _cbConfig) != DJSON_OK) {
-    FatalError("Cannot parse class patch \"%s\"", strConfigFile);
+  // load level config
+  DJSON_Block cbLevel;
+
+  if (ParseConfig(strConfigFile, cbLevel) != DJSON_OK) {
+    // can't parse the config
+    FatalError("Cannot parse class patch \"%s\"!", strConfigFile);
     return;
   }
+
+  // add level entries to global entries
+  _cbConfig.AddFrom(cbLevel, true);
 
   _ctConfigEntries = _cbConfig.Count();
 };
